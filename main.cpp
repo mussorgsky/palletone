78168a57ed2a8712dd74d21dfb2ee4bfa68d5f29
Mateusz <mateusz.gorzynski@gmail.com>
2018-01-10 12:21:37 +0100

    Shader source now included with the executable

--- main.cpp
+++ main.cpp
@@ -53,11 +53,36 @@ int main(int argc, char *argv[])
 		sf::Texture palette = image.paletteTexture();
 
 		sf::Shader shader;
-		shader.loadFromFile("shader.glsl", sf::Shader::Fragment);
-		shader.setUniform("resolution", sf::Vector2f((float)image.width, (float)image.height));
-		shader.setUniform("colors", (float)image.colors.size());
-		shader.setUniform("texture", cIndexes);
-		shader.setUniform("palette", palette);
+		const std::string shaderSource = R"(
+#version 300 es
+precision mediump float;
+
+uniform vec2 Resolution;
+uniform float ColCount;
+uniform sampler2D Texture;
+uniform sampler2D Palette;
+
+out vec4 Color;
+
+#define PI 3.1415
+
+void main() {
+    vec2 UV = gl_FragCoord.xy / Resolution;
+    UV.y = (UV.y - 0.5) * -1. + 0.5;
+    
+    int i = int(texture2D(Texture, UV).r * ColCount);
+    int h = int(sqrt(ColCount));
+
+    vec2 pUV = vec2(float(i % h), float(i / h)) / vec2(h);
+    
+	Color = vec4(texture2D(Palette, pUV));
+}
+)";
+		shader.loadFromMemory(shaderSource, sf::Shader::Fragment);
+		shader.setUniform("Resolution", sf::Vector2f((float)image.width, (float)image.height));
+		shader.setUniform("ColCount", (float)image.colors.size());
+		shader.setUniform("Texture", cIndexes);
+		shader.setUniform("Palette", palette);
 
 		sf::RectangleShape shape(sf::Vector2f((float)image.width, (float)image.height));
 		shape.setFillColor(sf::Color::Magenta);
@@ -89,7 +114,7 @@ int main(int argc, char *argv[])
 
 			image.cycleRanges(deltaTime.asSeconds());
 			palette = image.paletteTexture();
-			shader.setUniform("palette", palette);
+			shader.setUniform("Palette", palette);
 			// image.makeTexture(texturePixels);
 			// texture.update(texturePixels);
 
--- shader.glsl
+++ shader.glsl
@@ -1,18 +1,23 @@
 #version 300 es
 precision mediump float;
 
-uniform vec2 resolution;
-uniform float colors;
-uniform sampler2D texture;
-uniform sampler2D palette;
+uniform vec2 Resolution;
+uniform float ColCount;
+uniform sampler2D Texture;
+uniform sampler2D Palette;
 
-out vec4 color;
+out vec4 Color;
+
+#define PI 3.1415
 
 void main() {
-    vec2 UV = gl_FragCoord.xy / resolution;
+    vec2 UV = gl_FragCoord.xy / Resolution;
     UV.y = (UV.y - 0.5) * -1. + 0.5;
-    int i = int(texture2D(texture, UV).r * colors);
-    int h = int(sqrt(colors));
+    
+    int i = int(texture2D(Texture, UV).r * ColCount);
+    int h = int(sqrt(ColCount));
+
     vec2 pUV = vec2(float(i % h), float(i / h)) / vec2(h);
-	color = vec4(texture2D(palette, pUV));
+    
+	Color = vec4(texture2D(Palette, pUV));
 }
